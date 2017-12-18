9873f83b287feb532017e99b534047da2c26a3e0
Alexandr Kosarev <alex7r@ya.ru>
2017-12-18 15:40:08 +0300

    improve loader, update minimal compatible version

--- joomplaceXautoloader.php
+++ joomplaceXautoloader.php
@@ -7,12 +7,12 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-class PlgSystemJoomPlaceXAutoloader extends JPlugin
+class PlgSystemJoomPlaceXAutoloader extends Joomla\CMS\Plugin\CMSPlugin
 {
-    public function onAfterInitialise()
+    public function __construct($subject, array $config = array())
     {
-        JLoader::registerNamespace('JoomPlaceX',
-            JPATH_LIBRARIES . DIRECTORY_SEPARATOR . 'JoomPlaceX', false, false,
-            'psr4');
+        include_once JPATH_LIBRARIES . DIRECTORY_SEPARATOR . 'JoomPlaceX'. DIRECTORY_SEPARATOR . 'Loader.php';
+        Joomplace\X\Loader::boot();
+        parent::__construct($subject, $config);
     }
 }
