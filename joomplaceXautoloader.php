927c05242e385cb0b4acd38395c982fe476bb53b
Alexandr <alex7r@ya.ru>
2017-12-21 00:15:59 +0300

    checking signed commits
    Signed-off-by: Alexandr Kosarev <alex7r@ya.ru>

--- joomplaceXautoloader.php
+++ joomplaceXautoloader.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package     JoomPlaceX
- * @subpackage  System.autoloader
+ * @subpackage  System.Autoloader
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2017 JoomPlace Team, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
